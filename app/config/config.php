43ec8b1f86ca31c084381ebb1c28c5e58124381a
dreudi13 <jason.tubiana@gmail.com>
2017-01-24 16:21:11 +0100

    init config

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('SITE_NAME', 'Framework MVC' );
+define('ROOT', dirname(dirname(dirname(__FILE__))));
+define('APP', ROOT.DS.'app');
+define('LANG', ['en', 'fr']);
