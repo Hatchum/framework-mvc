912b4af10810b018ca14fe406239dfaed0f5cd2c
dreudi13 <jason.tubiana@gmail.com>
2017-01-25 11:45:01 +0100

    init database

---
+++ app/config/database.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ *  Define variables for connection to the database
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database_name');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php require 'vendor/autoload.php'; ?>
 
 <h1>It works!</h1>
+
+<?php
+$tab = [2, 5 ,9 ,7 ,1];
+$tab[-1]= 53;
+
+var_dump($tab);
+
