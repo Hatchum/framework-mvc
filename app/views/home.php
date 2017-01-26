2e588322882c2b031e5243a59882e022f7e9389f
dreudi13 <jason.tubiana@gmail.com>
2017-01-26 11:22:12 +0100

    url fixer :)

---
+++ app/views/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ultimate Blog</title>
+</head>
+<body>
+
+    <header>
+        <p>Home Page</p>
+        <h1>Ceci est développé grâce à notre super Framework :)!</h1>
+    </header>
+    
+    <main>
+    
+    </main>
+
+    <footer>
+    
+    </footer>
+
+</body>
+</html>
