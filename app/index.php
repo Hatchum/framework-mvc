0a6f40b5f1e8b26d8a142553b226adc9a6b103fa
gogobango <arthur.banckaert@gmail.com>
2017-01-26 13:47:31 +0100

    nada

--- app/index.php
+++ app/index.php
@@ -5,7 +5,13 @@
     if(isset($_GET['p']) && !empty($_GET['p'])) {
         $param = explode('/', $_GET['p']);
         $controller = ucfirst($param[0]);
-        $action = $param[1];
+
+        if(!empty($param[1])) {
+            $action = $param[1];
+        }
+        else {
+            $action = 'index';
+        }
 
         require(CONTROLLERS.$controller.'Controller.php');
 
@@ -13,10 +19,12 @@
 
         $controller = new $callController();
 
-        if(method_exists($controller, $action))
+        if(method_exists($controller, $action)) {
             $controller->$action();
-        else
-            require(ERRORS.'page_404.php');
+        }
+        else {
+            require(ERRORS . 'page_404.php');
+        }
     }
     else {
         require(VIEWS.'home.php');
