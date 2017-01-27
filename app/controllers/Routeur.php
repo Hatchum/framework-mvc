70a7494a6bbdd7bb30f0df5d2b936712d2f24776
gogobango <arthur.banckaert@gmail.com>
2017-01-27 15:11:07 +0100

    rooter terminated ?

--- app/controllers/Routeur.php
+++ app/controllers/Routeur.php
@@ -11,51 +11,51 @@ class Routeur
         array_push($this->controllers, "TutoController");
     }
 
-    public function routerRequete($route) {
+    public function routerRequete($param) {
         try {
-            if (isset($route['p']) && !empty($route['p'])) {
-                $param = explode('/', $route['p']);
+            if (isset($param) && !empty($param)) {
                 $this->envoiView($param);
             } else {
                 require(VIEWS . 'home.php');
             }
         }
         catch(Exception $e) {
-            $this->gererErreur($e);
+            $this->gererErreur($e->getMessage());
         }
     }
 
-   public function envoiView($param) {
-       $controller = ucfirst($param[0]);
+   public function envoiView(&$param) {
+       $controller = ucfirst($param['c']);
 
        if(!in_array($controller.'Controller', $this->controllers)){
-           require(ERRORS . 'page_404.php');
+           $this->gererErreur('Le controller \''.$controller.'\' n\'exite pas');
        }
        else {
-           if (!empty($param[1])) {
-               $action = $param[1];
-           }
-           else {
-               $action = 'index';
-           }
-
            require(CONTROLLERS . $controller . 'Controller.php');
 
            $callController = $controller . 'Controller';
 
            $controller = new $callController();
 
+           if (!empty($param['a'])) {
+               $action = $param['a'];
+           }
+           else {
+               $action = 'index';
+           }
+
            if (method_exists($controller, $action)) {
                $controller->$action();
            }
            else {
-               require(ERRORS . 'page_404.php');
+               $this->gererErreur('La methode \''.$action.'\' n\'exite pas');
            }
        }
    }
 
     public function gererErreur($e) {
-        extract(array('msgErreur' => $e->getMessage()));
+        $msg['erreur'] = array('msgErreur' => $e);
+        extract($msg);
         require(ERRORS . 'page_404.php');
     }
 }
